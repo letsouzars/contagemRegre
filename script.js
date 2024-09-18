5e8f00943c8d4d9ab53cdc8641083881ba172a23
Leticia Souza <133662327+letsouzars@users.noreply.github.com>
2024-09-18 11:52:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const countdownDate = new Date(new Date().getFullYear(), 11, 1).getTime();
+
+const updateCountdown = () => {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = String(days).padStart(2, '0');
+    document.getElementById("hours").innerText = String(hours).padStart(2, '0');
+    document.getElementById("minutes").innerText = String(minutes).padStart(2, '0');
+    document.getElementById("seconds").innerText = String(seconds).padStart(2, '0');
+
+    if (distance < 0) {
+        clearInterval(interval);
+        document.getElementById("timer").innerHTML = "Tempo Esgotado!";
+    }
+};
+
+const interval = setInterval(updateCountdown, 1000);
+updateCountdown();
